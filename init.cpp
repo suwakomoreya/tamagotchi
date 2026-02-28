2c36f7a2f6f093207869f0efbce999bcef4e3d34
suwakomoreya <tzunemain@gmail.com>
2026-02-28 11:22:33 -0800

    i think im figuring it out

--- init.cpp
+++ init.cpp
@@ -1,9 +1,25 @@
 #include <nlohmann/json.hpp>
 #include <fstream>
 #include <iostream>
-using json = nlohhman:json;
-using namespace std;
+#include <filesystem>
+using json = nlohmann::json;
+namespace fs = std::filesystem;
+
 int main(){
-    cout << "Tamagotchi"<<endl;
+    std::cout << "Tamagotchi"<< std::endl;
     
+    fs::path check = "tomodachis/info.json";
+    std::cout << "Current Path: " << std::filesystem::current_path() << std::endl;
+
+    if(!exists(check)){
+        json test{R"({
+            "listed": 0
+            })"_json};
+        std::fstream Info;
+        Info.open(R"(/home/tzunem/Desktop/tamagotchi/tamagotchi/tomodachis/info.json)",std::ios::out);
+        Info << test;
+        Info.close();
+    }
+        
+    return 0;
 }
