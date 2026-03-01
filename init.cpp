36771a0242b09022f3a492164a3cdbed40e66778
suwakomoreya <tzunemain@gmail.com>
2026-02-28 19:01:50 -0800

    spent way less time coding than the logs say, lotta testing on the web and reading documentation

--- init.cpp
+++ init.cpp
@@ -1,25 +1,45 @@
-#include <nlohmann/json.hpp>
+#include "json.hpp"
 #include <fstream>
 #include <iostream>
 #include <filesystem>
+
 using json = nlohmann::json;
 namespace fs = std::filesystem;
 
-int main(){
-    std::cout << "Tamagotchi"<< std::endl;
-    
-    fs::path check = "tomodachis/info.json";
-    std::cout << "Current Path: " << std::filesystem::current_path() << std::endl;
-
-    if(!exists(check)){
-        json test{R"({
-            "listed": 0
-            })"_json};
-        std::fstream Info;
-        Info.open(R"(/home/tzunem/Desktop/tamagotchi/tamagotchi/tomodachis/info.json)",std::ios::out);
-        Info << test;
+int main() {
+    fs::path infofile = "info.json"; 
+    std::fstream Info;
+    json infoObject;
+    std::cout << "Tamagotchi" << std::endl;
+    std::cout << "Current Path: " << fs::current_path() << std::endl;
+    /*make sure general info exists*/
+    if (!fs::exists(infofile)) {
+        json createInformation = { {"existingTomos", 0} };
+        Info.open(infofile, std::ios::out);
+        Info << createInformation.dump(4);  
         Info.close();
     }
+    /*check how many pets are saved*/
+    if(!fs::is_empty("tomodachis")){
+        Info.open(infofile, std::ios::in | std::ios::out);
+        Info >> infoObject;
+        infoObject["existingTomos"] = 0;
+        for(auto i : fs::directory_iterator("tomodachis"))
+            infoObject["existingTomos"] = infoObject.value("existingTomos", 0) + 1;
+        Info.seekp(0);
+        Info << infoObject.dump(4);  
+        Info.close();
+    }
+    else{
+        std::string petname;
         
+        std::cout << "Seems like you don't have a pet, so lets get you one." << std::endl;
+        std::cout << "What should be the name of your friend?: ";
+        std::cin >> petname;
+        
+        
+    }
+    /*create new pet if one doesnt exist*/
+
     return 0;
 }
