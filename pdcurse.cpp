ebba690923a16e292bb21677517a426760f2a816
suwakomoreya <tzunemain@gmail.com>
2026-03-14 00:34:16 -0700

    terminal size checker kinda sucks but id say it works now and finals are over so i can start working on the menu (actually)

--- pdcurse.cpp
+++ pdcurse.cpp
@@ -10,55 +10,57 @@ int main() {
     int termY,termX;
     WINDOW* screenScale = nullptr;
     WINDOW* menu = nullptr;
-    WINDOW* cover = nullptr;
     WINDOW* alerts = nullptr;
+    WINDOW* cover = nullptr;
     WINDOW** windows[] = {&screenScale,&menu,&cover,&alerts};
-    bool fire = true;
-    keypad(menu,TRUE);  
+    bool fire = true, alertupdate = false;
     auto updateMenu = [&menu](){
+        wclear(menu);
+        menu = newwin(21,30,1,4);
         mvwprintw(menu,1,10,"Tamagotchi");
         box(menu,0,0);
         wrefresh(menu);
     };
-    
+    auto updateCover = [&cover](){
+        wclear(cover);
+        cover = newwin(21,39,1,37);
+        box(cover,0,0);
+        wrefresh(cover);  
+    };
+    int tempX = -1,tempY = -1;
     while(true){
-        /*
-        for(WINDOW** i : windows)
-            delwin(*i);
-
-        for(WINDOW** i : windows)
-            wrefresh(*i);
-        
-        */
-        /*delete previous windows*/
         resize_term(0,0);
         getmaxyx(stdscr,termY,termX);
         screenScale = newwin(24,80,0,0);
-        menu = newwin(21,30,1,4);
-        cover = newwin(21,39,1,37);
-        alerts = newwin(1,termX,0,0);
-        keypad(menu,TRUE);
         
+        if(tempX < 0){
+            tempX = termX;
+            tempY = termY;
+        }
         /*windows*/
-        
         if(termX < 80 || termY < 24){
-            wprintw(alerts,"Size: %dx%d (Please resize your window!)", termX,termY);
-            wrefresh(alerts);
+            if(tempX !=termX || tempY != termY){
+                erase();
+                refresh();
+                alerts = newwin(1,termX,0,0);
+                werase(alerts);
+                wprintw(alerts,"Size: %dx%d (Please resize your window!)", termX,termY);
+                wrefresh(alerts);
+                tempX = termX;
+                tempY = termY;
+            }
             napms(50);
+            fire = true;
             continue;
         }
-        
-        int ch = getch();
-        if(ch == 'q'){
-            box(menu,0,0);
-            mvwprintw(menu,1,10,"Tamagotchi");
-            updateMenu();   
+        if(fire==true){
+            wclear(alerts);
+            updateMenu();
+            updateCover();    
         }
-        
+        alertupdate = false;
+        fire = false;
         napms(50);
     }
-    
-
-
     return 0;
 }
